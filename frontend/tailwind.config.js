8b85f90d57db1df10c1471388a275f1ee46fa2c9
kev <kektym@yahoo.com>
2024-04-11 09:29:54 -0400

    Initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html'], // Include your HTML file(s) here
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
